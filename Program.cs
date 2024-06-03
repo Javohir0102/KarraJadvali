38872da5116478857c5b9268abc50240c3dcce01
Javohir0102 <javohiranvarov96@gmail.com>
2024-06-03 23:54:32 +0500

    Aqilliy Karra Jadvali

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("1-oraliqni kiriting: ");
+short firstArgument = Convert.ToByte(Console.ReadLine());
+Console.Write("2-oraliqni kiriting: ");
+short lastArgument = Convert.ToByte(Console.ReadLine());
+
+for(int i = 1; i <= 9; i++)
+{
+    for(int j = firstArgument; j <= lastArgument; j++)
+    {
+        Console.Write($"{j}*{i}={(j*i), -9}");
+    }
+    Console.WriteLine();
+}
